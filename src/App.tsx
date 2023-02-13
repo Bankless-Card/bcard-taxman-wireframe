d43c743c8d29caf67614eec49b45871239809370
Tom Tranmer <tom@tomtranmer.com>
2023-02-13 13:41:52 -0500

    Updates to main data
    may need to remove web3js
    still looking for an easy way (without requiring wallet) to get the timstamp from the tx hash or block number
    implemented first look at coingecko history api calls(need to rate-limit)

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,30 @@
 import { Web3Button } from '@web3modal/react'
 import { useAccount } from 'wagmi'
 
+import { useState } from 'react'
+
 import { Account } from './components'
 import { Card } from './components'
 import { Clock } from './components'
 import { Btn } from './components'
 
+// web3 for tx data - can probably use wagmi for same
+// import Web3 from 'web3';
+// const web3 = new Web3();
+
 
 import { Alchemy, Network, AssetTransfersCategory } from 'alchemy-sdk';
 
+// get api key from .env
+const apiKey = process.env.REACT_APP_ALCHEMY_API_KEY;
+const apiHC = "szbJviwD1JXcAcbDNY0Mk7qn6uql-sN9";
+
+console.log(import.meta.env.VITE_SOME_KEY) // 123
+console.log(apiKey)
+
 // Optional Config object, but defaults to demo api-key and eth-mainnet.
 const settings = {
-  apiKey: 'szbJviwD1JXcAcbDNY0Mk7qn6uql-sN9', // Replace with your Alchemy API Key.
+  apiKey: apiHC, // Replace with your Alchemy API Key.
   network: Network.ETH_MAINNET, // Replace with your network.
 };
 
@@ -32,6 +45,123 @@ if(foo) {
 
 let heroImg = document.getElementById('heroImage');
 if(heroImg) { heroImg.className = cs.scaleWidth }
+
+function toggleSwitch(state:any){
+  // console.log('toggleSwitch', state)
+
+  let incomeOn = '<img src="./src/img/in.png" alt="Income" width="30" height="30" />';
+  let incomeOff = '<img src="./src/img/ni.png" alt="Income Off" height="30" width="50" />';
+
+  if(state === "INCOME"){
+    return incomeOn;
+  } else {
+    return incomeOff;
+  }
+}
+
+function getTokenLogo(asset:any){
+
+  if(asset === "BANK"){
+    return '<img src="./src/img/bankless-temp.png" alt="Bankless DAO" width="50" height="50" />';
+  }
+
+  return "Token Logo - " + asset;
+}
+
+function getTokenLabel(asset:any){
+
+  if(asset === "BANK"){
+    return "Bankless DAO";
+  }
+
+  return "Token Label - " + asset;
+}
+
+function displayTokenAmount(value:any, asset:any){
+
+  if(asset === "BANK"){
+    return  parseFloat(value).toFixed(3) + " " + asset;
+  }
+
+  return "Token Amount - " + value + " " + asset;
+}
+
+var gBankUsd = 0.0101;
+
+async function displayConvertAmount(value:any, asset:any){
+  // USD/FIAT value @ timefrom blockNum
+
+  if(asset === "BANK"){
+
+    let bankUSD = gBankUsd;   // 1 BANK = 0.01 USD (or newer price)
+
+    // use coingecko api to get price
+    // https://api.coingecko.com/api/v3/simple/price?ids=bankless-dao&vs_currencies=usd
+    fetch('https://api.coingecko.com/api/v3/coins/bankless-dao/market_chart/range?vs_currency=usd&from=1676310750&to=1676311028').then((response) => response.json()).then( async (data) => {
+      
+      console.log(data.prices[0][1]);   // this shoud be BANK price at closest timestamp to end
+
+      bankUSD = data.prices[0][1];
+      gBankUsd = bankUSD;   // update global var with new price
+
+      console.log((bankUSD*parseFloat(value)).toFixed(2) + " $USD")
+
+      return (bankUSD*parseFloat(value)).toFixed(2) + " $USD";
+    
+    });
+
+    return (bankUSD*parseFloat(value)).toFixed(2) + " $USD";
+
+
+
+  } else {
+    return "Convert Amount - " + value;
+  }
+
+}
+
+function doSomething(){
+  console.log("doSomething");
+
+  return "doSomething";
+}
+
+function moreTarget(event:any){
+  var caller = event.target;
+  console.log(caller+'more');
+}
+
+// function toggleItem(elem:any) {
+//   for (var i = 0; i < elem.length; i++) {
+//     elem[i].addEventListener("click", function(e:any) {
+//       var current = this;
+//       for (var i = 0; i < elem.length; i++) {
+//         if (current != elem[i]) {
+//           elem[i].classList.remove('active');
+//         } else if (current.classList.contains('active') === true) {
+//           current.classList.remove('active');
+//         } else {
+//           current.classList.add('active')
+//         }
+//       }
+//       e.preventDefault();
+//     });
+//   };
+// }
+// toggleItem(document.querySelectorAll('.link'));
+
+function handleClick(e:any) {
+  var curr = e.target.textContent;
+  var elem = document.querySelectorAll('#kiran');
+  for (var i = 0; i < elem.length; i++) {
+      if (elem[i].textContent === curr) {
+          elem[i].className = 'active';
+      } else {
+          elem[i].className = '';
+      }
+  }
+};
+
 
 async function alchemyGo(){
 
@@ -67,15 +197,21 @@ async function alchemyGo(){
   // estimated starting eth block for 2022
   // https://etherscan.io/block/13916169
 
-  const blockNumInt = 13916169;   //for 2022 eth mainnet hc
+  const blockNumInt = 13916169;   //for 2022 eth mainnet START - hc
   const startBlock = "0x" + blockNumInt.toString(16);   // format for 0x + hex
+
+  const endBlockNumInt = 16308155;   //for 2022 eth mainnet END - hc
+  const endBlock = "0x" + endBlockNumInt.toString(16);
 
   console.log(startBlock);
 
   const res = await alchemy.core.getAssetTransfers({
     fromBlock: startBlock,
+    toBlock: endBlock,
     toAddress: toAddress,
     excludeZeroValue: true,
+    // withMetadata: true,
+    // order: "desc",       // default asc for ascending
     category: [ AssetTransfersCategory.ERC20],
   });
 
@@ -85,6 +221,7 @@ async function alchemyGo(){
 
   // setup output object to DOM
   let output = document.getElementById("output");
+  output?.classList.add(cs.output);
 
   let objArr = res.transfers;   //this is the transaction list data object
   if(output){
@@ -108,10 +245,106 @@ async function alchemyGo(){
 
     // USD/FIAT value @ timefrom blockNum
 
+    // Income Label - Toggle SWITCH
+    
+    let incomeState = "NOT";
+    if(thisRow.asset === "BANK"){
+      incomeState = "INCOME";
+    }
+
+    let incomeBadge = toggleSwitch(incomeState); // 'Income Label - Toggle SWITCH';
+    let tokenLogo = getTokenLogo(thisRow.asset);   // token logo - '+thisRow.asset+'
+    let tokenLabel = getTokenLabel(thisRow.asset);   // token label - '+thisRow.asset+'
+    let tokenAmount = displayTokenAmount(thisRow.value,thisRow.asset);   // token amount - '+thisRow.value+'
+    let convertAmount = await displayConvertAmount(thisRow.value, thisRow.asset)// "USD/FIAT value @ timefrom blockNum";   // USD/FIAT value @ timefrom blockNum
+
+    let outBox = '<div class=' + cs.flexCont + '>\
+      <div class='+cs.row+'>\
+        <div class='+cs.col+'>\
+          '+incomeBadge+'\
+        </div>\
+        <div class='+cs.col+'>\
+          '+ tokenLogo + ' \
+        </div>\
+        <div class='+cs.col+'>\
+          <div class='+cs.row+'>\
+            Received\
+          </div>\
+          <div class='+cs.row+'>\
+            <strong>'+tokenLabel+'</strong>\
+          </div>\
+        </div>\
+        <div class='+cs.col+'>\
+          <div class='+cs.row+'>\
+            <span class='+cs.tokenAmount+'>'+tokenAmount+'</span>\
+          </div>\
+          <div class='+cs.row+'>\
+            <span class='+cs.convertAmount+'>'+convertAmount+'</span>\
+          </div>\
+        </div>\
+      </div>\
+    </div>';
+
+    
+    // let moreMsg = console.log("more button has been clicked");
+
+    // more needs to target the detail div
+    let moreBtn = "<h3><a href='#' title='show/hide detail view'>MORE</a></h3>";
+
+    // override
+    moreBtn = "<hr />";
+
+    // asset is the token symbol
+    // a defines the show/hide condition of the div
+    let a = thisRow.asset;
+
+    let listRow = "<li class='"+cs.tx + " " + a + " " + cs[incomeState] + "'>";
+    listRow += outBox + moreBtn;    // main tx output and more button to reveal detail
+    // listRow += getTokenLogo(thisRow.asset);
+
+    listRow += "<div class="+cs.detail+"><strong>DETAIL VIEW:"+a+" tx</strong><span style=float:right>X</span><br />";
+    listRow += "<div class="+cs.row+">";
+      listRow += "<div class="+cs.col+">";
+        listRow += getTokenLogo(a) + getTokenLabel(a);
+      listRow += "</div>";
+    
+    
+    listRow += "<div class="+cs.col+">";
+    listRow += "TokenAmt: <strong>" + thisRow.value +"</strong>" + "ConvertAmt: <strong>";
+    listRow += await displayConvertAmount(thisRow.value, a) +"</strong></div>";
+
+    listRow += "</div>";
+
+    // fetch trasaction detail based on tx hash
+    /*
+    let tx = "0x5da2844afb6826d4baed6ad7e8b536c00cbc921ac147773ad056f29f2e7c1762"
+    web3.eth.getTransaction(tx).blockNumber
+      1920050
+    web3.eth.getBlock(1920050).timestamp
+    */
+    
+    // let thisTimestamp = web3.eth.getBlock(1920050).timestamp;
 
 
-    let listRow = "<li> <strong>"+thisRow.asset+"</strong> - TokenAmt: <strong>" + thisRow.value +"</strong>";
-      listRow += "<br /> <a target='_blank' href=https://etherscan.io/tx/"+thisRow.hash+">etherscan_link</a> and blocknum: "+thisRow.blockNum+"</li>";
+    listRow += "<br/>DATE/TIME AS CAPTURED FROM BLOCKnum: "+thisRow.blockNum+" or TXhash: "+thisRow.hash+"<br /> <a target='_blank' class="+cs.buttonStyle+" href=https://etherscan.io/tx/"+thisRow.hash+">View TX on Etherscan</a> <br /><strong>From address: </strong><br />"+thisRow.from+"<br /><a href=https://etherscan.io/address/"+thisRow.from+" target=_blank class="+cs.buttonStyle+">View Sender</a><br />";
+
+    // default view for BANK token is income
+    if(a === "BANK"){
+      listRow += "<a onClick=handleClick(e)><img id=kiran src='./src/img/in.png' class='"+cs.activeIncome + " " + cs.toggleButton+"' alt=income  /></a>";   // active income image
+
+      listRow += "<img id=kiran src='./src/img/ni.png' alt=notincome class="+cs.toggleButton+" />";   // not income image
+    } else {
+      // non-income display
+      listRow += "<a onClick=handleClick(e)><img id=kiran src='./src/img/in.png' alt=income width=50 height=50 class="+cs.toggleButton+" /></a>";   // income image
+      listRow += "<img id=kiran src='./src/img/ni.png' alt=income width=60 height=40 class='"+cs.activeIncome+" "+cs.toggleButton+"' />";   // not income image
+    }
+
+    
+
+    listRow += "<br /><a href='#' class="+cs.buttonStyle+" title='revert & hide detail view'>Cancel</a>";
+    listRow += "<a href='#' class="+cs.buttonStyle+" title='save (auto) & hide detail view'>Save</a>"
+    
+    listRow += "</div></li>";   // end of detail & list item
 
         // should log to console the block detail
         // getBlock(thisRow.blockNumber);
@@ -268,8 +501,79 @@ function GetTransactions(props:any) : any {
 }
 
 function DaoSelect(props:any){
-  return (<div><input type="checkbox" id={props.token} name={props.name} value={props.name} />
-        <label htmlFor={props.name}> {props.name} ({props.token})</label></div>);
+
+  // console.log(props);
+
+  // get incoming state of token selection with props.tokenState
+
+  // console.log(props.tokenState);
+
+  const [TOKEN, setTOKEN] = useState(props.tokenState);
+  console.log(props.token, TOKEN);    // see current state
+
+  // using js lookup tokens in the transaction list
+  // if found, set the display to true
+  let allTxs = document.getElementsByClassName("tx");
+  // console.log(allTxs);
+
+  //  SAMPLE: this to hide all the transactions in the list
+
+  // NEXT UP: Make this conditional on the checkbox state
+  // if (TOKEN) { its an add, so no need to hide, just reveal
+  // } else { its a remove, so hide all the tokens first
+  if(!TOKEN){
+    [].forEach.call(allTxs, function (el:any) {
+      el.style.display = 'block';
+    });
+  } else {
+    [].forEach.call(allTxs, function (el:any) {
+      el.style.display = 'none';
+    });
+  }
+
+  // SAMPLE: this to show all matching transactions in the list
+  let myTxs = document.getElementsByClassName(props.token);
+  console.log(myTxs);
+  if(TOKEN){
+    // show all of the matching tokens
+    [].forEach.call(myTxs, function (el:any) {
+      el.style.display = 'block';
+    });
+  } else {
+    // remove all of the matching tokens
+    [].forEach.call(myTxs, function (el:any) {
+      el.style.display = 'none';
+    });
+  }
+  
+
+
+
+
+
+  function handleChange(e:any) {
+    setTOKEN(e.target.checked);
+  }
+
+  // !this.state.daoState
+
+  // toggle token state 
+  // let tokenSel = this.setDAOState(props.token);
+
+  // shows the checkbox for the DAO token
+  return (<div>
+    <input 
+      type="checkbox" 
+      id={props.token} 
+      name={props.name} 
+      // value={props.name} 
+      checked={TOKEN} 
+      onClick={handleChange} 
+    />
+    <label htmlFor={props.name}> 
+      {props.name} ({props.token})
+    </label>
+  </div>);
 }
 
 export function App() {
@@ -278,6 +582,15 @@ export function App() {
 
   //kickoff alchemyGo function
   const myFunc = alchemyGo();
+
+  // save state to track dao selection
+  // const [daoState, setDaoState] = useState({})
+
+  const [count, setCount] = useState(0);
+
+  // default true on token states to show all
+  // example with BANK token
+  const [BANK, setBANK] = useState(true);
 
   return (
     <div className={cs.container}>
@@ -358,7 +671,17 @@ export function App() {
 
         <a id="dao-page"></a>
         <h2 className={cs.red}>Which DAOs are you a part of?</h2>
-        <DaoSelect name="BanklessDAO" token="BANK" />
+        <DaoSelect name="BanklessDAO" token="BANK" tokenState={BANK}
+          // onClick={() => {
+          //   // toggle BANK state
+          //   setBANK(!BANK);
+
+          //   console.log(BANK);
+
+          //   // setCount(count => count + 1);
+          //   // console.log(count);
+          // }} 
+        />
         <DaoSelect name="1Inch" token="1INCH" />
 
         <DaoSelect name="Aragon" token="ANT" />
