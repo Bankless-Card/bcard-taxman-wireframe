e40addd4bb1359747bcc0dfa0901099a60e8b9f8
Tom Tranmer <tom@tomtranmer.com>
2023-02-21 15:19:47 -0500

    Pre-cleanup commit
    code in messy state and needs refactoring before it can be deployed.
    
    Showstopper Bug: Finish button returns 0 on build deployment

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,5 @@ export { Card } from './FuncComp'
 export { Clock } from './ClassComp'
 export { TxComp } from './TxComponent'
 export { Btn } from './BtnComp'
+
+export { bankFeed } from './bank_feed_2022'
