fa3059d3760d1ed417a5175e372f557ef543b309
Tom Tranmer <tom@tomtranmer.com>
2023-02-27 12:29:29 -0500

    Tx & Send
    principally tx flagging and initial testing for email send.

--- src/functions/getTokenLogo.tsx
+++ src/functions/getTokenLogo.tsx
@@ -1,10 +1,10 @@
 import cs from '../style.module.css'
-import { handleOpen } from './handleOpen';
+// import { handleOpen } from './handleOpen';
 
 export function getTokenLogo(asset:any) {
 
   if(asset === "BANK"){
-    return '<img class='+cs.tokenLogo+' src="./src/img/dao.png" alt="Bankless DAO" onClick='+handleOpen+' />';
+    return '<img class='+cs.tokenLogo+' src="./src/img/dao.png" alt="Bankless DAO" />';
   }
 
   // TODO: add more assets here
