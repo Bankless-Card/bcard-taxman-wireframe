5f4f106c36afd6a72b178861ef3f6cdf9a9c6842
Tom Tranmer <tom@tomtranmer.com>
2023-04-12 07:32:51 -0400

    Update getTokenLogo.tsx
    merge fixes

--- src/functions/getTokenLogo.tsx
+++ src/functions/getTokenLogo.tsx
@@ -6,19 +6,6 @@ export function getTokenLogo(asset:any) {
   } else if(asset === "WETH"){
     return "/img/token/eth-logo.png";
   } else if(asset === "DAI"){
-<<<<<<< HEAD
-    return '<img class='+cs.tokenLogo+' src="./src/img/dai-logo.png" alt="DAI Stablecoin" />';
-  } else if(asset === "1INCH"){
-    return '<img class='+cs.tokenLogo+' src="./src/img/1inch-logo.png" alt="1INCH Token" />';
-  } else if(asset === "ANT"){
-    return '<img class='+cs.tokenLogo+' src="./src/img/ant-logo.png" alt="Aragon Token" />';
-  } else if(asset === "MKR"){
-    return '<img class='+cs.tokenLogo+' src="./src/img/mkr-logo.png" alt="MKR Token" />';
-  } else if(asset === "POKT"){
-    return '<img class='+cs.tokenLogo+' src="./src/img/pokt-logo.png" alt="POKT Network Token" />';
-  } else if(asset === "POOL"){
-    return '<img class='+cs.tokenLogo+' src="./src/img/pool-logo.png" alt="Pooltogether Token" />';
-=======
     return "/img/token/dai-logo.png";
   } else if(asset === "1INCH"){
     return "/img/token/1inch-logo.png";
@@ -30,8 +17,20 @@ export function getTokenLogo(asset:any) {
     return "/img/token/pokt-logo.png";
   } else if(asset === "POOL"){
     return "/img/token/pool-logo.png";
->>>>>>> dev-merge
   }
+
+    //   return '<img class='+cs.tokenLogo+' src="./src/img/dai-logo.png" alt="DAI Stablecoin" />';
+  // } else if(asset === "1INCH"){
+  //   return '<img class='+cs.tokenLogo+' src="./src/img/1inch-logo.png" alt="1INCH Token" />';
+  // } else if(asset === "ANT"){
+  //   return '<img class='+cs.tokenLogo+' src="./src/img/ant-logo.png" alt="Aragon Token" />';
+  // } else if(asset === "MKR"){
+  //   return '<img class='+cs.tokenLogo+' src="./src/img/mkr-logo.png" alt="MKR Token" />';
+  // } else if(asset === "POKT"){
+  //   return '<img class='+cs.tokenLogo+' src="./src/img/pokt-logo.png" alt="POKT Network Token" />';
+  // } else if(asset === "POOL"){
+  //   return '<img class='+cs.tokenLogo+' src="./src/img/pool-logo.png" alt="Pooltogether Token" />';
+    
 
 
   // TODO: add more assets here
