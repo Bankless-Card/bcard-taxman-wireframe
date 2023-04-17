31ea1baa3d03551371b611e413bb2b17725fd8ba
Tom Tranmer <tom@tomtranmer.com>
2023-04-17 08:25:38 -0400

    New Tokens Guide
    how to add in new tokens via current code. - USDC Sample with Commits.

--- src/functions/getTokenLogo.tsx
+++ src/functions/getTokenLogo.tsx
@@ -3,10 +3,6 @@ export function getTokenLogo(asset:any) {
 
   if(asset === "BANK"){
     return "/img/token/dao.jpg";
-  } else if(asset === "WETH"){
-    return "/img/token/eth-logo.png";
-  } else if(asset === "DAI"){
-    return "/img/token/dai-logo.png";
   } else if(asset === "1INCH"){
     return "/img/token/1inch-logo.png";
   } else if(asset === "ANT"){
@@ -19,19 +15,14 @@ export function getTokenLogo(asset:any) {
     return "/img/token/pool-logo.png";
   }
 
-    //   return '<img class='+cs.tokenLogo+' src="./src/img/dai-logo.png" alt="DAI Stablecoin" />';
-  // } else if(asset === "1INCH"){
-  //   return '<img class='+cs.tokenLogo+' src="./src/img/1inch-logo.png" alt="1INCH Token" />';
-  // } else if(asset === "ANT"){
-  //   return '<img class='+cs.tokenLogo+' src="./src/img/ant-logo.png" alt="Aragon Token" />';
-  // } else if(asset === "MKR"){
-  //   return '<img class='+cs.tokenLogo+' src="./src/img/mkr-logo.png" alt="MKR Token" />';
-  // } else if(asset === "POKT"){
-  //   return '<img class='+cs.tokenLogo+' src="./src/img/pokt-logo.png" alt="POKT Network Token" />';
-  // } else if(asset === "POOL"){
-  //   return '<img class='+cs.tokenLogo+' src="./src/img/pool-logo.png" alt="Pooltogether Token" />';
-    
-
+  // non-DAO specific assets
+  else if(asset === "WETH"){
+    return "/img/token/eth-logo.png";
+  } else if(asset === "DAI"){
+    return "/img/token/dai-logo.png";
+  } else if(asset === "USDC"){
+    return "/img/token/usdc-logo.png";
+  }
 
   // TODO: add more assets here
 
