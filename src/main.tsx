775d303b557e5e30a906a0c6c720fb9d1440a88c
Tom Tranmer <tom@tomtranmer.com>
2024-04-11 14:25:30 -0400

    testing env shenanigans

--- src/main.tsx
+++ src/main.tsx
@@ -1,32 +1,7 @@
-// import { EthereumClient } from "@web3modal/ethereum";
-// import { Web3Modal } from "@web3modal/react";
-// wallet connect 1.0 removed due to expiry
-// replace with rainbowkit to connect and still use wagmi
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-// import { WagmiConfig } from "wagmi";
 
 import { App } from "./App";
-
-
-console.log(process.env);
-// import { chains, client, walletConnectProjectId } from "./wagmi";
-// const ethereumClient = new EthereumClient(client, chains);
-
-// import { WagmiConfig, createConfig, configureChains } from 'wagmi'
-// import { publicProvider } from 'wagmi/providers/public'
-// import { mainnet, optimism, polygon } from "wagmi/chains";
- 
-// const { chains, publicClient, webSocketPublicClient } = configureChains(
-//   [mainnet, optimism, polygon],
-//   [publicProvider()],
-// )
- 
-// const config = createConfig({
-//   autoConnect: true,
-//   publicClient,
-//   webSocketPublicClient,
-// })
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
