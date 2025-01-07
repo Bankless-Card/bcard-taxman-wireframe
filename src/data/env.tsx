b10e35b0e86cb77153dbeac4d353f744f5eefd79
Al Mithani <almithani@gmail.com>
2025-01-07 10:47:31 -0500

    removing old env var tests

--- src/data/env.tsx
+++ src/data/env.tsx
@@ -1,11 +1,3 @@
-// testing env vars
-console.log('Environment variables check:');
-console.log('ETH_ALCHEMY_API_KEY exists:', !!import.meta.env.VITE_ETH_ALCHEMY_API_KEY);
-console.log('MATIC_ALCHEMY_API_KEY exists:', !!import.meta.env.VITE_MATIC_ALCHEMY_API_KEY);
-console.log('OP_ALCHEMY_API_KEY exists:', !!import.meta.env.VITE_OP_ALCHEMY_API_KEY);
-console.log('ARBITRUM_ALCHEMY_API_KEY exists:', !!import.meta.env.VITE_ARBITRUM_ALCHEMY_API_KEY);
-console.log('BASE_ALCHEMY_API_KEY exists:', !!import.meta.env.VITE_BASE_ALCHEMY_API_KEY);
-
 export const REACT_APP_ALCHEMY_API_KEY = import.meta.env.VITE_REACT_APP_ALCHEMY_API_KEY  
 export const ELASTICMAIL_SECURETOKEN = import.meta.env.VITE_ELASTICMAIL_SECURETOKEN  
 
