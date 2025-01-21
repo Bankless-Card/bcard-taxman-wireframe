c30b0f89533f3fe89c84f57273a8ceff481d2dd5
Al Mithani <almithani@gmail.com>
2025-01-21 15:45:48 -0500

    gas fees and tx link in the csv

--- src/functions/exportData.jsx
+++ src/functions/exportData.jsx
@@ -4,7 +4,33 @@ export function exportData(country, txData) {
 
     // export data to CSV
     // first line is 'header'
-    let csvData = "timestamp, tokenName, tokenAmount, chain, fiatName, fiatAmount" + "\r\n";
+    let csvData = "timestamp, chain, tokenName, tokenAmount, fiatName, fiatAmount, from, to, txType, gasFee, gasFeeFiat, blockExplorerUrl" + "\r\n";
+
+    // Get block explorer URL based on chain
+    function getBlockExplorerUrl(chain, hash) {
+      switch(chain) {
+        case "Ethereum":
+          return `https://etherscan.io/tx/${hash}`;
+        case "Polygon":
+          return `https://polygonscan.com/tx/${hash}`;
+        case "Optimism":
+          return `https://optimistic.etherscan.io/tx/${hash}`;
+        case "Base":
+          return `https://basescan.org/tx/${hash}`;
+        case "Arbitrum":
+          return `https://arbiscan.io/tx/${hash}`;
+        default:
+          return `https://etherscan.io/tx/${hash}`;
+      }
+    }
+
+    function getGasActuallyPaid(tx){
+      if(!tx.incoming && tx.gasFee){
+        return [tx.gasFee, tx.gasFeeInFiat];
+      }
+
+      return [0,0];
+    }
 
     // console.log("GET FIAT CODE HERE FROM GLOBAL SETTINGS");
     let fiatCode = country;
@@ -32,10 +58,22 @@ export function exportData(country, txData) {
 
                   // convert unix time to human time
                   let dateOut = new Date(tx.unixT * 1000);
+                  let gasFees = getGasActuallyPaid(tx);
 
                   // generate the csv data for each tx row
-                  newLine = dateOut + "," + tx.asset + "," + tx.value + "," + tx.chain + "," + tx.fiatName + "," + tx.fiatValue + "\r\n";
-
+                  newLine = dateOut + ","; 
+                  newLine += tx.chain + ",";
+                  newLine += tx.asset + ","
+                  newLine += tx.value + ",";
+                  newLine += tx.fiatName + ",";
+                  newLine += tx.fiatValue + ",";
+                  newLine += tx.from + ",";
+                  newLine += tx.to + ",";
+                  newLine += tx.txType + ",";
+                  newLine += gasFees[0] + ",";
+                  newLine += gasFees[1] + ",";
+                  newLine += getBlockExplorerUrl(tx.chain, tx.hash) + "\r\n";
+                  
                   // only add to csvData if income
                   csvData += newLine;
 
